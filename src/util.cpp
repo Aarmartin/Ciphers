7bfad6fc1eac67afd02ce27482648894db1cebe6
Aaron Martin <aarmartin14@gmail.com>
2025-04-17 18:46:25 -0600

    Added comments for Cryptanalysis and Utilities

--- src/util.cpp
+++ src/util.cpp
@@ -6,6 +6,13 @@ using namespace std;
 #include <tuple>
 #include "../include/util.h"
 
+// Greatest Common Divisor
+// Takes in two integers and returns their greatest common divisor
+// Params:
+//  a - Integer
+//  b - Integer
+// Returns:
+//  GCD of a and b
 int gcd(int a, int b) {
     int t;
     while (b != 0) {
@@ -16,6 +23,13 @@ int gcd(int a, int b) {
     return a;
 }
 
+// Modular Inverse
+// Takes in two integers and returns the modular inverse of the first from the second
+// Params:
+//  a - Integer
+//  m - Integer
+// Returns:
+// The modular inverse of a mod m
 int modInverse(int a, int m) {
     int m0 = m;
     int x0 = 0;
@@ -72,6 +86,12 @@ bool isNumber(const string& input) {
     return true;
 }
 
+// Frequency
+// Takes in a string of text and returns a list of frequencies for each charact in the English language
+// Params:
+//  text - String
+// Returns:
+//  Frequency list of text for English
 vector<float> frequency(const string& text) {
     vector<int> count(26,0);
     vector<float> freq(26);
@@ -147,6 +167,12 @@ tuple<int,int> mostCommon(const vector<float>& freq) {
     return {imax, prevIMax};
 }
 
+// Index of Coincidence
+// Takes in a list of frequencies corresponding to a language and returns it's index of coincidence
+// Params:
+//  freq - Float Vector
+// Returns:
+//  The Index of Coincidence of freq
 float iofc(const vector<float>& freq) {
     float sum = 0;
     for (auto f: freq) {
@@ -155,6 +181,13 @@ float iofc(const vector<float>& freq) {
     return sum;
 }
 
+// Mutual Index of Coincidence
+// Takes in two lists of frequencies and returns their Mutual Index of Coincidence
+// Parames:
+//  freqA - Float Vector
+//  freqB - Float Vector
+// Returns
+//  Mutual Index of Coincidence of freqA and freqB
 float miofc(const vector<float>& freqA, const vector<float>& freqB) {
     float sum = 0;
     for (auto i = 0u; i < freqA.size(); i++) {
@@ -163,6 +196,13 @@ float miofc(const vector<float>& freqA, const vector<float>& freqB) {
     return sum;
 }
 
+// Frequency Shift
+// Shifts a frequency list, for MIC calculations on shifted ciphertext
+// Params:
+//  freq - Float Vector
+//  shift - Integer
+// Returns:
+//  Frequency vector freq shifted by shift
 vector<float> shift(vector<float> freq, int shift) {
     const size_t n = freq.size();
     std::size_t s = (shift % n + n) % n;
@@ -170,6 +210,15 @@ vector<float> shift(vector<float> freq, int shift) {
     return freq;
 }
 
+// Subtext Group
+// Takes in a string of text and returns a substring that contains every letter a specified step distance away, shifted by some amount
+// Ex. ABCDEFGHI, Step 3, Shift 1 returns BEH
+// Params:
+//  str - String
+//  step - Integer
+//  shift - Integer
+// Returns:
+//  Substring of str, of characters index minus shift mod step equal to 0
 string substring(const string& str, int step, int shift = 0) {
     string res;
     int j = 0;
