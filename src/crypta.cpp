6af49bed7aca3f669f307d4b9d121694f4563f36
Aaron Martin <aarmartin14@gmail.com>
2025-04-17 18:19:24 -0600

    Added Vigenere Cryptanalysis

--- src/crypta.cpp
+++ src/crypta.cpp
@@ -2,6 +2,9 @@ using namespace std;
 
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <cmath>
+#include <algorithm>
 #include "../include/util.h"
 #include "../include/ciphers.h"
 
@@ -29,9 +32,101 @@ void analyze_caesar_bruteforce(const string& text) {
         cout << "Decrypted with Key " << i << ": " << cDecrypt(text,i) << endl;
     }
 }
+/*
+void analyze_vigenere(const string& text) {
+
+    vector<float> english_frequency = getEnglishFrequency();
+    vector<float> freq;
+
+    string stext;
+
+    float ioc;
+    float max_ioc;
+    float max_max_ioc = 0;
+    float avg_ioc;
+    int kmax_max_ioc = 0;
+
+    // Key Length
+    for (int k = 2; k < 40; k++) {
+        avg_ioc = 0;
+        max_ioc = 0;
+        for (int s = 0; s < k; s++) {
+            stext = substring(text,k,s);
+            freq = frequency(stext);
+            ioc = iofc(freq);
+            cout << "IOFC: " << ioc <<  endl;
+            avg_ioc += ioc;
+            if (ioc > max_ioc) {
+                max_ioc = ioc;
+            }
+        }
+        avg_ioc = avg_ioc / k;
+        if (max_ioc > max_max_ioc) {
+            max_max_ioc = max_ioc;
+            kmax_max_ioc = k;
+        }
+        cout << "Key Length: " << k << " \tMax IOFC: " << max_ioc << " \tAverage IOFC: " << avg_ioc << endl;
+    }
+
+    cout << "Suspected Key Length: " << kmax_max_ioc << endl;
+
+
+}*/
 
 void analyze_vigenere(const string& text) {
-    cout << "Not Implemented" << endl;
+    int n = text.length();
+    float ic = iofc(frequency(text));
+
+    int fk = int(((0.0265 * n) / ((ic * (n-1)) - 0.0385 * n + 0.065)) + 0.5f);
+
+    cout << "Suspected Friedman Key Length: " << fk << " Adjusted: " << fk + int(sqrt(fk) + 4) << endl;
+
+    vector<float> freq;
+    float ioc;
+    float best_ioc = 0;
+    int best_k = 0;
+    float avg_ioc;
+    string stext;
+    // Key Length
+    for (int k = 2; k < fk + int(sqrt(fk) + 4); k++) {
+        avg_ioc = 0;
+        for (int s = 0; s < k; s++) {
+            stext = substring(text,k,s);
+            freq = frequency(stext);
+            ioc = iofc(freq);
+            avg_ioc += ioc;
+        }
+        avg_ioc = avg_ioc / k;
+        if (avg_ioc > best_ioc) {
+            best_ioc = avg_ioc;
+            best_k = k;
+        }
+        cout << "Key Length: " << k << " \tAverage IOFC: " << avg_ioc << endl;
+    }
+
+    cout << "Best key length found: " << best_k << endl;
+
+    string key;
+
+    float mutual;
+    float best_mutual = 0;
+    float ibest_mutual = 0;
+    for (int g = 0; g < best_k; g++) {
+        stext = substring(text,best_k,g);
+        best_mutual = 0;
+        ibest_mutual = 0;
+        for (int i = 0; i < 26; i++) {
+            mutual = miofc(frequency(stext),shift(getEnglishFrequency(),i));
+            if (mutual > best_mutual) {
+                best_mutual = mutual;
+                ibest_mutual = i;
+            }
+        }
+        key += char(ibest_mutual + 'A');
+    }
+
+    cout << "Key found: " << key << endl;
+    cout << "Decrypted Text: " << vDecrypt(text,key) << endl;
 }
 
 void analyze_affine(const string& text) {
