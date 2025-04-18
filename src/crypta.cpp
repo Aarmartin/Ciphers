c0c26958adf348604f0fee8b9c2eae6ad596fefc
Aaron <aaron.j.martin@du.edu>
2025-04-17 18:46:25 -0600

    Added comments for Cryptanalysis and Utilities

--- src/crypta.cpp
+++ src/crypta.cpp
@@ -8,6 +8,7 @@ using namespace std;
 #include "../include/util.h"
 #include "../include/ciphers.h"
 
+// Caesar Cryptanalysis function
 void analyze_caesar(const string& text) {
     
     vector<float> english_freq = getEnglishFrequency();
@@ -15,69 +16,40 @@ void analyze_caesar(const string& text) {
     int imax = 0;
     float max = 0;
     vector<float> freq = frequency(text);
+
+    // Loop through each possible shift
     for (int i = 0; i < 26; i++) {
+
+        // Compare Mutual Index of Coincidence of ciphertext with shifted english frequency
         float mutual = miofc(freq,shift(english_freq,i));
         cout << "Mutual found for " << i << ": " << mutual << endl;
+
+        // Track best MIC
         if (mutual > max) {
             max = mutual;
             imax = i;
         }
     }
+
+    // Decrypt
     cout << "Index Found: " << imax << endl;
     cout << "Decrypted Text: " << cDecrypt(text,imax) << endl;
 }
 
+// Caesar Cryptanalysis funcion through bruteforce
 void analyze_caesar_bruteforce(const string& text) {
     for (int i = 1; i < 26; i++) {
         cout << "Decrypted with Key " << i << ": " << cDecrypt(text,i) << endl;
     }
 }
-/*
+
+// Vigenere Cryptanalysis function
 void analyze_vigenere(const string& text) {
 
-    vector<float> english_frequency = getEnglishFrequency();
-    vector<float> freq;
-
-    string stext;
-
-    float ioc;
-    float max_ioc;
-    float max_max_ioc = 0;
-    float avg_ioc;
-    int kmax_max_ioc = 0;
-
-    // Key Length
-    for (int k = 2; k < 40; k++) {
-        avg_ioc = 0;
-        max_ioc = 0;
-        for (int s = 0; s < k; s++) {
-            stext = substring(text,k,s);
-            freq = frequency(stext);
-            ioc = iofc(freq);
-            cout << "IOFC: " << ioc <<  endl;
-            avg_ioc += ioc;
-            if (ioc > max_ioc) {
-                max_ioc = ioc;
-            }
-        }
-        avg_ioc = avg_ioc / k;
-        if (max_ioc > max_max_ioc) {
-            max_max_ioc = max_ioc;
-            kmax_max_ioc = k;
-        }
-        cout << "Key Length: " << k << " \tMax IOFC: " << max_ioc << " \tAverage IOFC: " << avg_ioc << endl;
-    }
-
-    cout << "Suspected Key Length: " << kmax_max_ioc << endl;
-
-
-}*/
-
-void analyze_vigenere(const string& text) {
+    // Find Friendman approximation of Vigenere key length
     int n = text.length();
-    float ic = iofc(frequency(text));
-
-    int fk = int(((0.0265 * n) / ((ic * (n-1)) - 0.0385 * n + 0.065)) + 0.5f);
+    float ic = iofc(frequency(text));                                           // Index of Coincidence
+    int fk = int(((0.0265 * n) / ((ic * (n-1)) - 0.0385 * n + 0.065)) + 0.5f);  // Friedman formula
 
     cout << "Suspected Friedman Key Length: " << fk << " Adjusted: " << fk + int(sqrt(fk) + 4) << endl;
 
@@ -87,16 +59,26 @@ void analyze_vigenere(const string& text) {
     int best_k = 0;
     float avg_ioc;
     string stext;
-    // Key Length
+
+    // Loop through each possible key length up to Friedman key length plus a small amount
     for (int k = 2; k < fk + int(sqrt(fk) + 4); k++) {
         avg_ioc = 0;
+
+        // Loop through each subtext group
+        // Ex. ABCDEFGHI, Key length 3, Loop 1: ADG, Loop 2: BEH, Loop 3: CFI
         for (int s = 0; s < k; s++) {
             stext = substring(text,k,s);
             freq = frequency(stext);
+
+            // Calculate Index of Coincidence
             ioc = iofc(freq);
             avg_ioc += ioc;
         }
+        
+        // Take average IC
         avg_ioc = avg_ioc / k;
+
+        // Track best IC for each key length
         if (avg_ioc > best_ioc) {
             best_ioc = avg_ioc;
             best_k = k;
@@ -111,10 +93,14 @@ void analyze_vigenere(const string& text) {
     float mutual;
     float best_mutual = 0;
     float ibest_mutual = 0;
+
+    // Loop through each subtext group for best key length found
     for (int g = 0; g < best_k; g++) {
         stext = substring(text,best_k,g);
         best_mutual = 0;
         ibest_mutual = 0;
+
+        // Perform Caesar Cryptanalysis on subtext
         for (int i = 0; i < 26; i++) {
             mutual = miofc(frequency(stext),shift(getEnglishFrequency(),i));
             if (mutual > best_mutual) {
@@ -122,24 +108,36 @@ void analyze_vigenere(const string& text) {
                 ibest_mutual = i;
             }
         }
+
+        // Add Character to Key
         key += char(ibest_mutual + 'A');
     }
 
+    // Decrypt
     cout << "Key found: " << key << endl;
     cout << "Decrypted Text: " << vDecrypt(text,key) << endl;
 }
 
+// Affine Cryptanalysis Function
 void analyze_affine(const string& text) {
     
     float miofcv;
     float mmiofc = 0;
     int ma = 0, mb = 0;
     string plaintext;
+
+    // Loop through each possible value for b in affine key (a,b)
     for (int i = 0; i < 26; i++) {
+        // Loop through each possible value of a, where a mod 26 has a GCD of 1 (coprime)
         for (int j = 0; j < 26; j++) {
             if (gcd(j,26) == 1) {
+                //Decrypt Plaintext
                 plaintext = aDecrypt(text,j,i);
+
+                // Calculate Mutual Index of Coincidence
                 miofcv = miofc(frequency(plaintext),getEnglishFrequency());
+
+                // Track best MIC
                 if (miofcv > mmiofc) {
                     mmiofc = miofcv;
                     ma = j;
@@ -149,22 +147,9 @@ void analyze_affine(const string& text) {
         }
     }
 
+    // Decrypt
     cout << "Key: (" << ma << "," << mb << ")" << endl
          << "Decrypted Text: " << aDecrypt(text,ma,mb) << endl;
-
-    /*
-    vector<float> freq = frequency(text);
-
-    auto [m1, m2] = mostCommon(freq);
-
-    cout << "Most Common Found: " << m1 << ", " << char(m1 + 'a') << ", " << freq[m1] << endl;
-    cout << "Second Most Common Found: " << m2 << ", " << char(m2 + 'a') << ", " << freq[m2] << endl;
-
-    auto [a, b] = affine_key('e','t',char(m1 + 'a'), char(m2 + 'a'));
-
-    cout << "Key found: (" << a << "," << b << ")" << endl;
-    cout << "Decrypted Text: " << aDecrypt(text, a, b) << endl;
-    */
 }
 
 int main(int argc, char** argv) {
